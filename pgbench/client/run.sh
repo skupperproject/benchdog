16f93757e6efe01261b1dbc7fd7e971adc476f51
Justin Ross <jross@apache.org>
2021-09-28 09:41:59 -0400

    WIP

--- pgbench/client/run.sh
+++ pgbench/client/run.sh
@@ -4,4 +4,29 @@ if [[ -z $DATABASE_SERVER ]]; then
     DATABASE_SERVER=localhost
 fi
 
-pgbench --jobs 10 --client 10 --time 30 --host $DATABASE_SERVER --username test --progress 2
+echo "### 1 client ###"
+echo
+
+for i in {1..3}; do
+    pgbench --jobs 1 --client 1 --time 30 --host $DATABASE_SERVER --username test --progress 2 --select-only
+    echo
+    sleep 10
+done
+
+echo "### 10 clients ###"
+echo
+
+for i in {1..3}; do
+    pgbench --jobs 10 --client 10 --time 30 --host $DATABASE_SERVER --username test --progress 2 --select-only
+    echo
+    sleep 10
+done
+
+echo "### 100 clients ###"
+echo
+
+for i in {1..3}; do
+    pgbench --jobs 100 --client 100 --time 30 --host $DATABASE_SERVER --username test --progress 2 --select-only
+    echo
+    sleep 10
+done
