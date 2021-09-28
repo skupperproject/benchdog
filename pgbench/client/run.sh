80a73631797883db43aad6eb08382e8e7961f967
Justin Ross <jross@apache.org>
2021-09-28 13:32:14 -0400

    WIP

--- pgbench/client/run.sh
+++ pgbench/client/run.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
 
-if [[ -z $DATABASE_SERVER ]]; then
-    DATABASE_SERVER=localhost
+if [[ -z $PGBENCH_SERVER_HOST ]]; then
+    PGBENCH_SERVER_HOST=localhost
 fi
 
-echo "### 1 client ###"
+common_args="--host $PGBENCH_SERVER_HOST --time 30 --username test --progress 2 --select-only"
+
+echo "# Running pgbench against host $PGBENCH_SERVER_HOST on $(date)"
+echo
+
+echo "## 1 client"
 echo
 
 for i in {1..3}; do
-    pgbench --jobs 1 --client 1 --time 30 --host $DATABASE_SERVER --username test --progress 2 --select-only
+    echo "### Run $i"
+    echo
+    pgbench --jobs 1 --client 1 $common_args
     echo
     sleep 10
 done
 
-echo "### 10 clients ###"
+echo "## 10 clients"
 echo
 
 for i in {1..3}; do
-    pgbench --jobs 10 --client 10 --time 30 --host $DATABASE_SERVER --username test --progress 2 --select-only
+    echo "### Run $i"
+    echo
+    pgbench --jobs 10 --client 10 $common_args
     echo
     sleep 10
 done
 
-echo "### 100 clients ###"
+echo "## 100 clients"
 echo
 
 for i in {1..3}; do
-    pgbench --jobs 100 --client 100 --time 30 --host $DATABASE_SERVER --username test --progress 2 --select-only
+    echo "### Run $i"
+    echo
+    pgbench --jobs 100 --client 100 $common_args
     echo
     sleep 10
 done
