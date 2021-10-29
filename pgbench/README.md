fc150f65ef537dc994a29f362b70951f63c21215
Justin Ross <jross@apache.org>
2021-10-29 08:08:30 -0400

    WIP

--- pgbench/README.md
+++ pgbench/README.md
@@ -1,3 +1,7 @@
-## Running the PostgreSQL container on OpenShift
+## Running the server on OpenShift
 
     oc adm policy add-scc-to-group anyuid system:authenticated
+
+## Running the client
+
+    kubectl run -it --rm --env BENCHDOG_SERVER_HOST=169.63.182.185 --env BENCHDOG_SERVER_PORT=5432 --env BENCHDOG_DURATION=1 --env BENCHDOG_ITERATONS=3 --image quay.io/ssorj/benchdog-pgbench-client pgbench-client
--- pgbench/client/python/main.py
+++ pgbench/client/python/main.py
@@ -1,3 +1,6 @@
+import json as _json
+import numpy as _numpy
+
 from plano import *
 
 ENV["PGUSER"] = "postgres"
@@ -5,17 +8,19 @@ ENV["PGPASSWORD"] = "postgres"
 
 host = ENV.get("BENCHDOG_SERVER_HOST", "localhost")
 port = ENV.get("BENCHDOG_SERVER_PORT", "5432")
+duration = int(ENV.get("BENCHDOG_DURATION", 10))
+iterations = int(ENV.get("BENCHDOG_ITERATIONS", 5))
 
 await_port(port, host=host)
 
-def run_pgbench(scale):
+def run_pgbench(clients):
     args = [
         "pgbench",
-        "--jobs", str(scale),
-        "--client", str(scale),
-        "--time", ENV.get("BENCHDOG_DURATION", "10"),
+        "--jobs", str(clients),
+        "--client", str(clients),
         "--host", host,
         "--port", port,
+        "--time", str(duration),
         "--progress", "2",
         "--select-only",
         "--log",
@@ -23,8 +28,85 @@ def run_pgbench(scale):
 
     run(args)
 
-run_pgbench(1)
-run_pgbench(10)
-run_pgbench(100)
+def process_pgbench_logs(file, clients):
+    dtype = [
+        ("client_id", _numpy.uint64),
+        ("transaction_no", _numpy.uint64),
+        ("time", _numpy.uint64),
+        ("script_no", _numpy.uint64),
+        ("time_epoch", _numpy.uint64),
+        ("time_us", _numpy.uint64),
+    ]
 
-    # f"pgbench --jobs {scale} --client {scale} --time {duration} --host 5ddcad63-us-east.lb.appdomain.cloud --port 5432 --progress 2 --select-only --log")
+    records = _numpy.loadtxt(file, dtype=dtype)
+    records.sort(order=("time_epoch", "time_us"))
+
+    start_time = records[0][4] + records[0][5] / 1000000 - records[0][2] / 1000000
+    end_time = records[-1][4] + records[-1][5] / 1000000
+
+    duration = end_time - start_time
+    operations = len(records)
+    throughput = operations / duration
+
+    latencies = records["time"]
+    average = _numpy.mean(latencies)
+    percentiles = _numpy.percentile(latencies, (50, 99))
+
+    data = {
+        "clients": clients,
+        "duration": round(duration, 2),
+        "operations": operations,
+        "throughput": round(throughput, 2),
+        "latency": {
+            "average": round(average / 1000, 2),
+            "50": round(percentiles[0] / 1000, 2),
+            "99": round(percentiles[1] / 1000, 2),
+        },
+    }
+
+    append("results", _json.dumps(data) + "\n")
+
+def run_config(clients):
+    remove("results")
+
+    for i in range(iterations):
+        sleep(duration)
+        run_pgbench(clients)
+        run("cat pgbench_log.* > pgbench_log", shell=True)
+
+        if get_file_size("pgbench_log") != 0:
+            process_pgbench_logs("pgbench_log", clients)
+
+        run("rm pgbench_log*", shell=True)
+
+    print()
+    print("XXX 1")
+
+    print(read("results"))
+
+    print("XXX 2")
+
+    results = list()
+    throughputs = list()
+
+    with open("results") as f:
+        for line in f:
+            result = parse_json(line)
+            results.append(result)
+            throughputs.append(result["throughput"])
+
+    index = throughputs.index(_numpy.percentile(throughputs, 50, interpolation="nearest"))
+
+    result = results[index]
+
+    print(result)
+
+    append("report", _json.dumps(result) + "\n")
+
+if __name__ == "__main__":
+    run_config(1)
+    run_config(10)
+    run_config(100)
+    print()
+    print("XXX 3")
+    print(read("report"))
