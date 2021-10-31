961297a48e010f76f8f0b3861ef8a69e0ef4bb0d
Justin Ross <jross@apache.org>
2021-10-31 08:45:27 -0400

    WIP

--- wrk/client/python/benchdog.py
+++ wrk/client/python/benchdog.py
@@ -1,20 +1,66 @@
 from plano import *
 
+import json as _json
 import numpy as _numpy
 
-def summarize(data):
-    summary = dict()
+def load_config():
+    return Namespace(host=ENV.get("BENCHDOG_SERVER_HOST", "localhost"),
+                     port=ENV.get("BENCHDOG_SERVER_PORT", "8080"),
+                     duration=int(ENV.get("BENCHDOG_DURATION", 10)),
+                     iterations=int(ENV.get("BENCHDOG_ITERATIONS", 5)))
 
-    for config in ("1", "10", "100"):
-        results = data[config]
-        throughputs = [x["operations"] / x["duration"] for x in results]
+def report(config, data):
+    print()
+    print("## Configuration")
+    print()
+
+    print(f"Server: {config.host}:{config.port}")
+    print(f"Duration: {config.duration} {plural('second', config.duration)}")
+    print(f"Iterations: {config.iterations}")
+
+    print()
+    print("## Data")
+    print()
+
+    print(_json.dumps(data))
+
+    results = dict()
+
+    for scenario in ("1", "10", "100"):
+        scenario_data = data[scenario]
+        throughputs = [x["operations"] / x["duration"] for x in scenario_data]
         throughput = _numpy.percentile(throughputs, 50, interpolation="nearest")
         index = throughputs.index(throughput)
-        result = results[index]
+        result = scenario_data[index]
 
-        summary[config] = {
+        results[scenario] = {
             "throughput": round(throughput, 2),
             "latency": result["latency"],
         }
 
-    return summary
+    columns = "{:>7}  {:>12}  {:>12}  {:>12}  {:>12}"
+
+    print()
+    print("## Results")
+    print()
+
+    print(columns.format("CLIENTS", "THROUGHPUT", "LATENCY AVG", "LATENCY 50%", "LATENCY 99%"))
+
+    for scenario in ("1", "10", "100"):
+        result = results[scenario]
+        latency = result["latency"]
+
+        print(columns.format(scenario,
+                             "{:,.2f}".format(result["throughput"]),
+                             "{:,.2f}".format(latency["average"]),
+                             "{:,.2f}".format(latency["50"]),
+                             "{:,.2f}".format(latency["99"])))
+
+    print()
+    print("## Notes")
+    print()
+
+    print(" - Throughput is operations per second")
+    print(" - Latencies are in milliseconds")
+
+    print()
--- wrk/client/python/main.py
+++ wrk/client/python/main.py
@@ -3,10 +3,7 @@ import numpy as _numpy
 
 from benchdog import *
 
-host = ENV.get("BENCHDOG_SERVER_HOST", "localhost")
-port = ENV.get("BENCHDOG_SERVER_PORT", "8080")
-duration = int(ENV.get("BENCHDOG_DURATION", 10))
-iterations = int(ENV.get("BENCHDOG_ITERATIONS", 5))
+config = load_config()
 
 def run_wrk(clients):
     args = [
@@ -14,32 +11,32 @@ def run_wrk(clients):
         "--header", "'Connection: close'",
         "--threads", str(clients),
         "--connections", str(clients),
-        "--duration", str(duration),
+        "--duration", str(config.duration),
         "--latency",
         "--script", "main.lua", # This writes to result.json
-        f"http://{host}:{port}/",
+        f"http://{config.host}:{config.port}/",
     ]
 
     run(args)
 
     return read_json("result.json")
 
-def run_config(clients):
+def run_scenario(clients):
     results = list()
 
-    for i in range(iterations):
-        sleep(min((10, duration)))
+    for i in range(config.iterations):
+        sleep(min((10, config.duration)))
         results.append(run_wrk(clients))
 
     return results
 
 if __name__ == "__main__":
-    await_port(port, host=host)
+    await_port(config.port, host=config.host)
 
-    result = {
-        "1": run_config(1),
-        "10": run_config(10),
-        "100": run_config(100),
+    data = {
+        "1": run_scenario(1),
+        "10": run_scenario(10),
+        "100": run_scenario(100),
     }
 
-    print(emit_json(summarize(result)))
+    report(config, data)
