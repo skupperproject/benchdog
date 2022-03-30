b4124e961c0685d3eda99d7ccd964d4cc5f27f4f
Justin Ross <jross@apache.org>
2022-03-30 17:07:04 -0400

    WIP

--- wrk/client/main.lua
+++ wrk/client/main.lua
@@ -1,6 +1,6 @@
 done = function(summary, latency, requests)
     file = io.open("result.json", "w")
     file:write(string.format("{\"duration\": %.02f, \"operations\": %d, \"latency\": {\"average\": %.02f, \"50\": %.02f, \"99\": %.02f}}\n",
-                             summary.duration / 1000000, summary.requests, latency.mean, latency:percentile(50), latency:percentile(99)))
+                             summary.duration / 1000000, summary.requests, latency.mean / 1000, latency:percentile(50) / 1000, latency:percentile(99) / 1000))
     file:close()
 end
