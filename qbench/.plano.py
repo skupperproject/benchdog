bc81aefb2feaf2df7f17e348af95754ff62f6792
Justin Ross <jross@apache.org>
2023-08-11 13:17:27 -0400

    WIP

---
+++ qbench/.plano.py
@@ -0,0 +1,29 @@
+from plano import *
+
+@command
+def run_():
+    with working_dir("client"):
+        run("plano build")
+
+    with working_dir("server"):
+        run("plano build")
+
+    server = start("server/qbench-server")
+
+    client = start("client/qbench-client")
+
+    sleep(5)
+
+    kill(client)
+    kill(server)
+
+    wait(client)
+    wait(server)
+
+    run("cat qbench.*.log > qbench.log", shell=True)
+
+    count = call("wc -l qbench.log", shell=True).split()[0]
+
+    print()
+    print(">>> {:c,} <<<".format(int(count) / 5))
+    print()
